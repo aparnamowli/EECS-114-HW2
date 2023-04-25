3ee5c7cf554fc76164c95b43f8b8517c6ac69e26
aparnamowli <amowli@uci.edu>
2023-04-24 20:18:52 -0700

    HW2 driver and README

---
+++ queue.h
@@ -0,0 +1,17 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+class Queue
+{
+  public:
+  Queue();
+  /* implement copy constructor, assignment, destructor if needed */
+  void enqueue(float value);
+  float dequeue();
+
+  private:
+  /* declare your data */
+};
+
+
+#endif
